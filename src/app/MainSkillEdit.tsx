18203e4187e4c06fdb6ffb20052bcfc0da1eeee4
wogikaze <kouse2319@gmail.com>
2023-12-06 19:36:49 +0900

    fix level

--- src/app/MainSkillEdit.tsx
+++ src/app/MainSkillEdit.tsx
@@ -3,16 +3,24 @@ import React, { useEffect, useState } from "react";
 import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField } from "@mui/material";
 
 export function MainSkillEdit(props: { open: boolean; onClose: () => void; pokemon: Pokemon }) {
-  const [level, setLevel] = useState(props.pokemon.mainSkillLevel);
+  const [level, setLevel] = useState<string>(props.pokemon.mainSkillLevel.toString());
+  const handleChange = (value: string) => {
+    const parsed = parseInt(value, 10);
+    if (Number.isNaN(parsed)) {
+      return "";
+    }
+    return parsed;
+  };
   useEffect(() => {
-    setLevel(props.pokemon.mainSkillLevel);
+    setLevel(props.pokemon.mainSkillLevel.toString());
   }, [props]);
   async function onClickItem() {
-    if (level >= 1 && level <= 6) props.pokemon.mainSkillLevel = level;
+    const parsed = parseInt(level, 10);
+    if (parsed >= 1 && parsed <= 6) props.pokemon.mainSkillLevel = parsed;
     close();
   }
   function close() {
-    setLevel(1);
+    setLevel("1");
     props.onClose();
   }
   return (
@@ -26,7 +34,7 @@ export function MainSkillEdit(props: { open: boolean; onClose: () => void; pokem
             <DialogContentText>現在のメインスキル：{props.pokemon.mainSkill}</DialogContentText>
             <DialogContentText>Lv.{props.pokemon.mainSkillLevel}</DialogContentText>
           </div>
-          <TextField autoFocus margin="dense" id="level" label="レベル" type="" value={level} onChange={(e) => setLevel(parseInt(e.target.value))} fullWidth />
+          <TextField autoFocus margin="dense" id="level" label="レベル" type="" value={level} onChange={(e) => handleChange(e.target.value)} fullWidth />
         </DialogContent>
         <DialogActions>
           <Button onClick={props.onClose}>キャンセル</Button>
--- src/app/levelEdit.tsx
+++ src/app/levelEdit.tsx
@@ -1,18 +1,27 @@
 import { Pokemon } from "../types";
 import React, { useEffect, useState } from "react";
 import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField } from "@mui/material";
+import { PasswordRounded } from "@mui/icons-material";
 
 export function LevelEdit(props: { open: boolean; onClose: () => void; pokemon: Pokemon }) {
-  const [level, setLevel] = useState(props.pokemon.level);
+  const [level, setLevel] = useState<string>(props.pokemon.level.toString());
+  const handleChange = (value: string) => {
+    const parsed = parseInt(value, 10);
+    if (Number.isNaN(parsed)) {
+      return "";
+    }
+    return parsed;
+  };
   useEffect(() => {
-    setLevel(props.pokemon.level);
+    setLevel(props.pokemon.level.toString());
   }, [props]);
   async function onClickItem() {
-    if (level >= 1) props.pokemon.level = level;
+    const parsed = parseInt(level, 10);
+    if (parsed >= 1) props.pokemon.level = parsed;
     close();
   }
   function close() {
-    setLevel(1);
+    setLevel("1");
     props.onClose();
   }
   return (
@@ -22,7 +31,7 @@ export function LevelEdit(props: { open: boolean; onClose: () => void; pokemon: 
         <DialogContent>
           <DialogContentText>選択されたポケモン：{props.pokemon.name}</DialogContentText>
           <DialogContentText>現在のレベル：{props.pokemon.level}</DialogContentText>
-          <TextField autoFocus margin="dense" id="level" label="レベル" type="" value={level} onChange={(e) => setLevel(parseInt(e.target.value))} fullWidth />
+          <TextField autoFocus margin="dense" id="level" label="レベル" type="" value={handleChange(level)} onChange={(e) => setLevel(e.target.value)} fullWidth />
         </DialogContent>
         <DialogActions>
           <Button onClick={props.onClose}>キャンセル</Button>
